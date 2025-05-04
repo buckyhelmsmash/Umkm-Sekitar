60771629f7f3352b15e8d62138a7bfa8c18eb3a4
todos <todo.siahaan@gmail.com>
2025-05-05 00:27:32 +0700

    Fix Profile Screen

--- app/src/main/java/com/example/umkm_sekitar/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/umkm_sekitar/ui/screen/profile/ProfileScreen.kt
@@ -1,32 +1,41 @@
 package com.example.umkm_sekitar.ui.screen.profile
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil3.compose.rememberAsyncImagePainter
 import com.example.umkm_sekitar.ui.screen.auth.AuthViewModel
-
 
 @Composable
 fun ProfileScreen(
@@ -39,69 +48,96 @@ fun ProfileScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start
+                .verticalScroll(rememberScrollState())
+                .background(Color.White)
         ) {
-            ElevatedCard(
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 6.dp
-                ),
+            Box(
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
+                    .fillMaxWidth()
+                    .height(160.dp)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color(0xFFB2CCFF), Color.White)
+                        )
+                    )
             ) {
-                Text(
-                    text = "Elevated",
+                Image(
+                    painter = rememberAsyncImagePainter(user.photoUrl),
+                    contentDescription = "Profile Photo",
                     modifier = Modifier
-                        .padding(16.dp),
-                    textAlign = TextAlign.Center,
+                        .size(100.dp)
+                        .align(Alignment.BottomCenter)
+                        .clip(CircleShape)
                 )
             }
-            Text(
-                text = "Profil",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(bottom = 24.dp)
-            )
 
+            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                SectionTitle("Profil")
+                ProfileItem("Nama Lengkap", user.displayName ?: "-") {}
+                ProfileItem("Email", user.email ?: "-") {}
 
-            Text(text = "Nama Lengkap", style = MaterialTheme.typography.labelMedium)
-            Text(
-                text = user.displayName ?: "User",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFF5F5F5), RoundedCornerShape(8.dp))
-                    .padding(12.dp)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Email
-            Text(text = "Email", style = MaterialTheme.typography.labelMedium)
-            Text(
-                text = user.email ?: "Not available",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFF5F5F5), RoundedCornerShape(8.dp))
-                    .padding(12.dp)
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-
-
-            Button(
-                onClick = { viewModel.signOut(context) },
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
+                Spacer(modifier = Modifier.height(10.dp))
+                SectionTitle("Akun")
+                CustomProfileActionItem(
+                    label = "Keluar",
+                    description = "Keluar dari akun",
+                    icon = Icons.Filled.ExitToApp,
+                    iconTint = Color.Red,
+                    labelColor = Color.Red
                 ) {
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Keluar", color = Color.White)
+                    viewModel.signOut(context)
                 }
+                Divider()
             }
         }
+    }
+}
+
+@Composable
+fun SectionTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.labelLarge,
+        modifier = Modifier.padding(vertical = 8.dp)
+    )
+}
+
+@Composable
+fun ProfileItem(label: String, value: String, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp)
+    ) {
+        Text(text = label, style = MaterialTheme.typography.bodyLarge)
+        Text(text = value, style = MaterialTheme.typography.bodyMedium, color = Color.Gray)
+        Divider(modifier = Modifier.padding(top = 12.dp))
+    }
+}
+
+@Composable
+fun CustomProfileActionItem(
+    label: String,
+    description: String,
+    icon: ImageVector,
+    iconTint: Color = Color.Black,
+    labelColor: Color = Color.Black,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(text = label, style = MaterialTheme.typography.bodyLarge, color = labelColor)
+            Text(text = description, style = MaterialTheme.typography.bodyMedium, color = Color.Gray)
+        }
+        Icon(imageVector = icon, contentDescription = null, tint = iconTint)
     }
 }
 
